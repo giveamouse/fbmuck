8fef04dc9e711ef03e8e691f8d1098350e3e54aa
revar <revar>
2001-08-03 08:05:52 +0000

    Updated copyright messages.

--- include/copyright.h
+++ include/copyright.h
@@ -1,63 +1,48 @@
-/* $Header$
- * $Log: copyright.h,v $
- * Revision 1.2  2000/03/29 12:21:01  revar
- * Reformatted all code into consistent format.
- * 	Tabs are 4 spaces.
- * 	Indents are one tab.
- * 	Braces are generally K&R style.
- * Added ARRAY_DIFF, ARRAY_INTERSECT and ARRAY_UNION to man.txt.
- * Rewrote restart script as a bourne shell script.
- *
- * Revision 1.1.1.1  1999/12/12 07:28:12  revar
- * Initial Sourceforge checkin, fb6.00a29
- *
- * Revision 1.1.1.1  1999/12/12 07:28:12  foxen
- * Initial FB6 CVS checkin.
- *
- * Revision 1.1  1996/06/17 17:29:45  foxen
- * Initial revision
- *
- * Revision 5.12  1994/01/06  03:18:09  foxen
- * Version 5.12
- *
- * Revision 5.1  1993/12/17  00:35:54  foxen
- * initial revision.
- *
- * Revision 1.2  90/07/19  23:08:47  casie
- * Removed comment log from top.
- * 
- * 
- */
 /* -*-C-*-
 
-Copyright (c) 1989, 1990 by David Applegate, James Aspnes, Timothy Freeman,
-			    Bennet Yee, and Na Choon Piaw (lachesis).
+The majority of this code is Copyright (c) 1991-2001 by Fuzzball Software
+  Licensed under the GNU Public License.
 
-This material was developed by the above-mentioned authors.
-Permission to copy this software, to redistribute it, and to use it
-for any purpose is granted, subject to the following restrictions and
-understandings.
 
-1. Any copy made of this software must include this copyright notice
-in full.
+Some parts of this code Copyright (c) 1989-1990 by David Applegate,
+    James Aspnes, Timothy Freeman, Bennet Yee, and Na Choon Piaw (lachesis),
+    under the collowing terms:
 
-2. Users of this software agree to make their best efforts (a) to
-return to the above-mentioned authors any improvements or extensions
-that they make, so that these may be included in future releases; and
-(b) to inform the authors of noteworthy uses of this software.
+	This material was developed by the above-mentioned authors.
+	Permission to copy this software, to redistribute it, and to use it
+	for any purpose is granted, subject to the following restrictions and
+	understandings.
 
-3. All materials developed as a consequence of the use of this
-software shall duly acknowledge such use, in accordance with the usual
-standards of acknowledging credit in academic research.
+	1. Any copy made of this software must include this copyright notice
+	in full.
 
-4. The authors have made no warrantee or representation that the
-operation of this software will be error-free, and the authors are
-under no obligation to provide any services, by way of maintenance,
-update, or otherwise.
+	2. Users of this software agree to make their best efforts (a) to
+	return to the above-mentioned authors any improvements or extensions
+	that they make, so that these may be included in future releases; and
+	(b) to inform the authors of noteworthy uses of this software.
 
-5. In conjunction with products arising from the use of this material,
-there shall be no use of the names of the authors, of Carnegie-Mellon
-University, nor of any adaptation thereof in any advertising,
-promotional, or sales literature without prior written consent from
-the authors, Carnegie-Mellon University and Bell Communications Research
-in each case. */
+	3. All materials developed as a consequence of the use of this
+	software shall duly acknowledge such use, in accordance with the usual
+	standards of acknowledging credit in academic research.
+
+	4. The authors have made no warrantee or representation that the
+	operation of this software will be error-free, and the authors are
+	under no obligation to provide any services, by way of maintenance,
+	update, or otherwise.
+
+	5. In conjunction with products arising from the use of this material,
+	there shall be no use of the names of the authors, of Carnegie-Mellon
+	University, nor of any adaptation thereof in any advertising,
+	promotional, or sales literature without prior written consent from
+	the authors, Carnegie-Mellon University and Bell Communications Research
+	in each case.
+ 
+ 
+Parts of this code, in particular the dictionary based compression code,
+  are copyright by Dr. Cat, 1995, and are NOT covered by the above copyright
+  statements.  Dr. Cat has given permission for the code to be used in the
+  FB distribution of the TinyMUCK server.
+
+ */
+
+
