75a619e692b9e4f45bc643c342523f0dc671779a
revar <revar>
2006-11-21 09:57:01 +0000

    Updated version to 6.07.

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
-#define VERSION "Muck2.2fb6.06cvs"
+#define VERSION "Muck2.2fb6.07"
 
 #ifdef DEFINE_HEADER_VERSIONS
 
 #ifndef versionh_version
 #define versionh_version
-const char *version_h_version = "$RCSfile$ $Revision: 1.27 $";
+const char *version_h_version = "$RCSfile$ $Revision: 1.28 $";
 #endif
 #else
 extern const char *version_h_version;
